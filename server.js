f779c8d8ebf85fdd8866b9823d8c3d755992c914
TARISO <toni.ariso@gmail.com>
2021-02-14 15:24:04 +0100

    Post With AXIOS

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const {save_user_information, get_total_amount} = require('./models/server_db');
+const {
+  save_user_information,
+  get_total_amount,
+} = require('./models/server_db');
 const path = require('path');
 const publicPath = path.join(__dirname, './public');
 
@@ -9,28 +12,25 @@ const publicPath = path.join(__dirname, './public');
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
-app.post('/', async (req,res) => {
+app.post('/post_info', async (req, res) => {
   var email = req.body.email;
   var amount = req.body.amount;
 
-  if(amount <= 1) {
+  if (amount <= 1) {
     return_info = {};
     return_info.error = true;
-    return_info.message = "The amount should be greater than 1";
+    return_info.message = 'The amount should be greater than 1';
     return res.send(return_info);
   }
 
-    var result = await save_user_information({"amount": amount, "email": email});
-    res.send(result);
-
-
-});
-
-app.get('/get_total_amount', async(req,res)=>{
-  var result = await get_total_amount();
+  var result = await save_user_information({ amount: amount, email: email });
   res.send(result);
 });
 
+app.get('/get_total_amount', async (req, res) => {
+  var result = await get_total_amount();
+  res.send(result);
+});
 
 app.listen(3000, () => {
   console.log('server is running on port 3000');
